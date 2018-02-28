ce7c5e89e0f839942c30c78b0319cab64fb2ce14
Brian Kendall <brian@briankendall.net>
2018-02-28 16:33:20 -0500

    Code cleanup and updated gitignore

--- src/extension.ts
+++ src/extension.ts
@@ -11,23 +11,11 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "indenttobracket" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-        console.log('Wut wut!');
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
     context.subscriptions.push(
         vscode.workspace.onDidChangeTextDocument(
             changeEvent => { activateOnEnter(changeEvent) }
         )
     );
-
-    context.subscriptions.push(disposable);
 }
 
 function activateOnEnter(changeEvent: vscode.TextDocumentChangeEvent) {
@@ -43,14 +31,6 @@ function activateOnEnter(changeEvent: vscode.TextDocumentChangeEvent) {
         console.log('\n\nEnter press!!');
         onEnterPress(changeEvent);
     }
-}
-
-function positionToString(p: vscode.Position) {
-    return ("(l: " + p.line + " c: " + p.character + ")");
-}
-
-function rangeToString(r: vscode.Range) {
-    return ("start: " + positionToString(r.start) + " end: " + positionToString(r.end));
 }
 
 function allBracketsInString(s: string) {
@@ -69,33 +49,6 @@ function lineEndsWithOpenBracket(line: string) {
     var regex = /(\(|\[|{)\s*$/g;
     return line.search(regex) !== -1;
 }
-
-/*
-
-    blah = asdf()
-    wut(asdf, asdf, (thingy,
-                     foo = asdfdfd(123, 45, {1, 2, 3,
-                                             [4, 5, 6,
-                                              (hi there),
-                                              5],
-                                             'asdfasdf'}),
-                     bloo))
-    wut(asdf, asdf, (thingy,
-                     foo = asdfdfd(123, 45, {1, 2, 3,
-                                             [4, 5, 6,
-                                              (hi there,
-                                               asdf,
-                                               asdf,
-                                               asdfd),
-                                              1232,
-                                              334]},
-                                   asdfasdf)
-                     asdfasdf,
-                     asdfasdfasdf))
-                                
-
-
-*/
 
 interface IObjectWithStringKey { [key: string]: string; }
 interface IObjectWithIntKey { [key: string]: number; }
@@ -128,8 +81,6 @@ function findIndentationPositionInLineAndKeepTallyOfBrackets(line: string, talli
 function bracketTalliesIndicateAllBracketsClosed(tallies: IObjectWithIntKey) {
     for(var key in tallies) {
         if (tallies.hasOwnProperty(key)) {
-            //console.log('bracketTalliesIndicateAllBracketsClosed key: ' + key);
-            
             if (tallies[key] !== 0) {
                 return false;
             }
@@ -137,11 +88,6 @@ function bracketTalliesIndicateAllBracketsClosed(tallies: IObjectWithIntKey) {
     }
     
     return true;
-}
-
-function indentationLevelOfLine(line: string) {
-    var regex = /(\(|\[|{)\s*$/g;
-    return line.search(regex) !== -1;
 }
 
 function findIndentationPosition(document: vscode.TextDocument, lastLineNumber: number) {
@@ -157,22 +103,14 @@ function findIndentationPosition(document: vscode.TextDocument, lastLineNumber: 
     for(var currentLineNumber = lastLineNumber; currentLineNumber >= 0; --currentLineNumber) {
         var currentLine = document.lineAt(currentLineNumber).text;
         var indentationIndex = findIndentationPositionInLineAndKeepTallyOfBrackets(currentLine, tallies);
-        console.log("Considering line " + currentLineNumber + " : " + currentLine);
-        console.log("  tallies:");
-        
-        for(var key in tallies) {
-            if (tallies.hasOwnProperty(key)) {
-                console.log("    " + key + " : " + tallies[key]);
-            }
-        }
         
         if (indentationIndex !== null) {
-            console.log("  found indentation index: " + indentationIndex);
+            //console.log("  found indentation index: " + indentationIndex);
             return indentationIndex;
         }
         
         if (bracketTalliesIndicateAllBracketsClosed(tallies)) {
-            console.log("  all brackets closed");
+            //console.log("  all brackets closed");
             if (currentLineNumber !== lastLineNumber) {
                 return document.lineAt(currentLineNumber).firstNonWhitespaceCharacterIndex;
             } else {
@@ -190,16 +128,7 @@ function onEnterPress(changeEvent: vscode.TextDocumentChangeEvent) {
     if (editor === undefined) {
         return;
     }
-
-    for(let i = 0; i < changeEvent.contentChanges.length; ++i) {
-        //console.log("changes " + i + ": " + rangeToString(changeEvent.contentChanges[i].range) + "->" + rangeToString(changeEvent.contentChanges[i].range) + " : " + changeEvent.contentChanges[i].text.length);
-        //console.log("change length: " + changeEvent.contentChanges[i].rangeLength);
-    }
-
-    let document = editor.document;
-    let position = editor.selection.active;
-
-    //console.log("position1: " + position.line + " " + position.character);
+    
     setTimeout(maybeIndentToBracket, 0);
 }
 
@@ -213,15 +142,12 @@ function maybeIndentToBracket() {
     let document = editor.document;
     let position = editor.selection.active;
     
-    //console.log("position2: " + position.line + " " + position.character);
-    
     if (position.line === 0) {
         return;
     }
     
-    let prevLine = document.lineAt(position.line-1);
     let indentationPosition = findIndentationPosition(document, position.line-1);
-    console.log("indent to: " + indentationPosition);
+    console.log("indenting to: " + indentationPosition);
     
     if (indentationPosition === null) {
         return;
@@ -234,13 +160,8 @@ function maybeIndentToBracket() {
     } else {
         editor.edit(function (edit: vscode.TextEditorEdit): void {
             edit.delete(new vscode.Range(new vscode.Position(position.line, indentationPosition), position));
-        }).then(success => {
-            console.log("deletion edit success: " + success);
         });
     }
-    
-    
-    
 }
 
 // this method is called when your extension is deactivated
